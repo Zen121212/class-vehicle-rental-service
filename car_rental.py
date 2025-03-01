a831be20904d4403002549ee0111d2dbb7e74850
Zein Kassem <zein.s.kassem@gmail.com>
2025-03-01 23:30:12 +0200

    fix polymorphism for show_vehicle_info()

--- car_rental.py
+++ car_rental.py
@@ -4,11 +4,11 @@ class Vehicle:
         self.model = model
         self.year = year
         self.__rental_price_per_day = rental_price_per_day
-    def display_info(self):
-        if isinstance(self, Car):
-            print(f"Car: {self.brand} {self.model}, Year: {self.year}, Seats: {self.seating_capacity}, Rental Price: ${self.get_rental_price()}/day")
-        elif isinstance(self, Bike):
-            print(f"Bike: {self.brand} {self.model}, Year: {self.year}, Engine: {self.engine_capacity}cc, Rental Price:${self.get_rental_price()}/day")
+    # def display_info(self):
+    #     if isinstance(self, Car):
+    #         print(f"Car: {self.brand} {self.model}, Year: {self.year}, Seats: {self.seating_capacity}, Rental Price: ${self.get_rental_price()}/day")
+    #     elif isinstance(self, Bike):
+    #         print(f"Bike: {self.brand} {self.model}, Year: {self.year}, Engine: {self.engine_capacity}cc, Rental Price:${self.get_rental_price()}/day")
     def calculate_rental_cost(self, days):
         print(f"Rental cost for {self.brand} {self.model} for {days} days: ${self.__rental_price_per_day * days}")
     def get_rental_price(self):
@@ -22,10 +22,14 @@ class Car(Vehicle):
     def __init__(self, brand, model, year,seating_capacity,rental_price_per_day):
         super().__init__(brand, model,year, rental_price_per_day)
         self.seating_capacity = seating_capacity
+    def display_info(self):
+        print(f"Car: {self.brand} {self.model}, Year: {self.year}, Seats: {self.seating_capacity}, Rental Price: ${self.get_rental_price()}/day")
 class Bike(Vehicle):
     def __init__(self, brand, model, year, engine_capacity,rental_price_per_day):
         super().__init__(brand, model,year, rental_price_per_day)
         self.engine_capacity = engine_capacity
+    def display_info(self):
+            print(f"Bike: {self.brand} {self.model}, Year: {self.year}, Engine: {self.engine_capacity}cc, Rental Price:${self.get_rental_price()}/day")
 def show_vehicle_info(vehicle):
     vehicle.display_info()
 
